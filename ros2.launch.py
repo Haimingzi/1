84ad4bfdf44748bdf390e77073f00f2d53b3d3f5
王海明 <155439623+Haimingzi@users.noreply.github.com>
2024-07-28 11:25:54 +0800

    Update ros2.launch.py

--- ros2.launch.py
+++ ros2.launch.py
@@ -6,36 +6,61 @@ def generate_launch_description():
     
     turtlesim_node = Node(
         package="turtlesim",
-        executable="turtlesim_node",
-        name="turtlesim",
-        output="screen",
-
-        
+        executable="turtlesim_node"
     )
 
    
     turtle_walker = Node(
         package="ROS2_XXXX",
         executable="turtle_walker",
-         
+        parameters=[{"turtle_name":"Tom_XXXX"}]
     )
  
-    controller = Node(
-        package="ROS2_XXXX",
-        executable="controller",
+    # controller = Node(
+    #     package="ROS2_XXXX",
+    #     executable="controller",
          
+    # )
+    # turtle_pos_sub = Node(
+    #     package="ROS2_XXXX",
+    #     executable="turtle_pos_sub",
+         
+    # )
+
+    #分别广播两只乌龟相对于world的坐标变换
+
+    tf_broadcaster1 = Node(
+        package="ROS2_XXXX",
+        executable="tf_broadcaster",
+        name="tf_broadcaster1",
+        parameters=[{"turtle":"turtle1"}]
+
     )
-    turtle_pos_sub = Node(
+
+    tf_broadcaster2 = Node(
         package="ROS2_XXXX",
-        executable="turtle_pos_sub",
-         
+        executable="tf_broadcaster",
+        name="tf_broadcaster2",
+        parameters=[{"turtle":"Tom_XXXX"}]
     )
     
+
+    turtle_tf_listener=Node(
+        package="ROS2_XXXX",
+        executable="turtle_tf_listener",
+        parameters=[{"father_frame":"Tom_XXXX","child_frame":"turtle1"}]
+    )
+
     launch_description = LaunchDescription([
+  
+        # turtle_pos_sub,
+        turtlesim_node,
         turtle_walker,
-        controller,
-        turtle_pos_sub,
-        turtlesim_node
+        # controller,
+        tf_broadcaster1,
+        tf_broadcaster2,
+        turtle_tf_listener
+
     ])
 
     return launch_description
